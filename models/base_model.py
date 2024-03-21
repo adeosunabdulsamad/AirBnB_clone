b1df297840d52d951bf81e13a7dc238b12d04f8c
Adeosun Abdulsamad <adeosunabdulsamad25@gmail.com>
2024-03-21 16:02:11 -0700

    Updated the base model and the corresponding test case

--- models/base_model.py
+++ models/base_model.py
@@ -3,18 +3,26 @@ import uuid
 import datetime
 
 class BaseModel:
-    def __init__(self):
-        self.id = str(uuid.uuid1())
-        self.created_at = datetime.datetime.now()
-        self.updated_at = datetime.datetime.now()
+     def __init__(self, *args, **kwargs):
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    setattr(self, key, datetime.datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
+                else:
+                    if key != '__class__':
+                            setattr(self, key, value)
+        else:
+                self.id = str(uuid.uuid1())
+                self.created_at = datetime.datetime.now()
+                self.updated_at = datetime.datetime.now()
 
-    def __str__(self):
+     def __str__(self):
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
     
-    def save(self):
+     def save(self):
         self.updated_at = datetime.datetime.now()
 
-    def to_dict(self):
+     def to_dict(self):
         new_dict = self.__dict__.copy()
         new_dict['__class__'] = self.__class__.__name__
         new_dict['created_at'] = self.created_at.isoformat()
--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,33 +1,36 @@
 #1/usr/bin/python3
 import unittest
 from models.base_model import BaseModel
+import datetime
 
 class TestBaseModel(unittest.TestCase):
     def setUp(self):
-        self.model = BaseModel()
+        self.base_model = BaseModel()
 
-    def test_init(self):
-        self.assertIsNotNone(self.model.id)
-        self.assertIsNotNone(self.model.created_at)
-        self.assertIsNotNone(self.model.updated_at)
+    def test_init_with_args(self):
+        base_model = BaseModel(id='123', created_at='2022-01-01T12:00:00.000')
+        self.assertEqual(base_model.id, '123')
+        self.assertEqual(base_model.created_at, datetime.datetime(2022, 1, 1, 12, 0, 0))
+        self.assertEqual(base_model.updated_at, datetime.datetime(2022, 1, 1, 12, 0, 0))
 
-    def test_str(self):
-        expected_output = f"[BaseModel] ({self.model.id}) {self.model.__dict__}"
-        self.assertEqual(str(self.model), expected_output)
+    def test_init_without_args(self):
+        self.assertIsNotNone(self.base_model.id)
+        self.assertIsInstance(self.base_model.created_at, datetime.datetime)
+        self.assertIsInstance(self.base_model.updated_at, datetime.datetime)
 
     def test_save(self):
-        old_updated_at = self.model.updated_at
-        self.model.save()
-        self.assertNotEqual(old_updated_at, self.model.updated_at)
+        old_updated_at = self.base_model.updated_at
+        self.base_model.save()
+        self.assertNotEqual(self.base_model.updated_at, old_updated_at)
 
     def test_to_dict(self):
         expected_dict = {
-            '__class__': 'BaseModel',
-            'id': self.model.id,
-            'created_at': self.model.created_at.isoformat(),
-            'updated_at': self.model.updated_at.isoformat()
+            'id': self.base_model.id,
+            'created_at': self.base_model.created_at.isoformat(),
+            'updated_at': self.base_model.updated_at.isoformat(),
+            '__class__': 'BaseModel'
         }
-        self.assertEqual(self.model.to_dict(), expected_dict)
+        self.assertEqual(self.base_model.to_dict(), expected_dict)
 
 if __name__ == '__main__':
     unittest.main()
